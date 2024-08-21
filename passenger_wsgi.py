6caf99aba78902f4ed1f272babe80ef9049d143b
leroysb <leroybuliro@gmail.com>
2024-08-21 18:17:41 +0300

    wsgi bug fix

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,10 @@
+import sys
+import os
+
+# Add the project directory to the PYTHONPATH
+sys.path.append('/home/daiorg/repositories/d4ai-django')
+
+# Set the Django settings module environment variable
+os.environ['DJANGO_SETTINGS_MODULE'] = 'd4ai_app.settings'
+
 from d4ai_app.wsgi import application
